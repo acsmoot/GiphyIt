660efbbe652870d1161e08a017ab2933b0f4ee25
Angela <acsmoot14@gmail.com>
2018-10-09 23:06:39 -0500

    resolved 404-not found third added https back

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ function getCarGiphies() {
 
   console.log(carName);
   // // Make call to giphy
-  var queryURL="http://api.giphy.com/v1/gifs/search?q=" +
+  var queryURL="https://api.giphy.com/v1/gifs/search?q=" +
     carName +
     "&api_key=" +
     api_key +
